9844ae44f145fd6084c3dd87d4f1e7b30c1fe624
Jomancia <delciamanuel94@gmail.com>
2025-10-30 09:33:14 +0100

    erro npo css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,jpeg}",
-  ],
+  content: [],
   theme: {
     extend: {},
   },
